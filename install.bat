65dc1e0b230e6e8a14fdaad2d668dd046c8715df
jerry-harm <1030621717@qq.com>
2023-11-05 19:13:17 +0800

    修复无deploy的bug

--- install.bat
+++ install.bat
@@ -22,7 +22,7 @@ echo 安装winrar
 start /wait .\app\winrar.exe /S
 
 echo 安装office
-start /wait ".\app\Office Tool\Office Tool Plus.Console.exe" deploy /add O365HomePremRetail_zh-cn /edition 64
+".\app\Office Tool\Office Tool Plus.Console.exe" deploy /add O365HomePremRetail_zh-cn /edition 64
 echo 激活程序
 ".\app\Office Tool\Office Tool Plus.Console.exe" ospp /inslicid ProPlus2021Volume
 ".\app\Office Tool\Office Tool Plus.Console.exe" ospp /sethst kms.loli.best
